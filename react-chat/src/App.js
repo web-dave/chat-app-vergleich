f323f35e86b293c9930e8d58b5790427ae0dcb82
web-dave <dave@webdave.de>
2023-02-27 17:01:51 +0100

    React lazy loading

--- react-chat/src/App.js
+++ react-chat/src/App.js
@@ -1,17 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { Suspense } from "react";
 import Home from "./components/Home";
-import ChatPage from "./components/ChatPage";
 import socketIO from "socket.io-client";
 
+const ChatPage = React.lazy(() => import("./components/ChatPage"));
 const socket = socketIO.connect("http://localhost:4000");
 function App() {
   return (
     <BrowserRouter>
       <div>
-        <Routes>
-          <Route path="/" element={<Home socket={socket} />}></Route>
-          <Route path="/chat" element={<ChatPage socket={socket} />}></Route>
-        </Routes>
+        <Suspense fallback={<>...</>}>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Suspense fallback={<>...</>}>
+                  <Home socket={socket} />
+                </Suspense>
+              }
+            ></Route>
+            <Route path="/chat" element={<ChatPage socket={socket} />}></Route>
+          </Routes>
+        </Suspense>
       </div>
     </BrowserRouter>
   );
